61be966d593f49628a401dac5356179c3f3979bf
Mitch Williams <mitch@socialcast.com>
2012-04-18 13:00:04 -0700

    Made the logging module more middleware-friendly

--- lib/oauth2/provider/logging.rb
+++ lib/oauth2/provider/logging.rb
@@ -10,16 +10,31 @@ module OAuth2::Provider
       :fatal => Logger::Severity::FATAL
     }
 
+    def set_log_context(context)
+      @logging_context = context
+    end
+
+    def clear_log_context
+      @logging_context = nil
+    end
+
     def log(message, extra = {})
-      return unless OAuth2::Provider.logger
+      rack_env = self.respond_to?(:env) ? env : nil
+      logger = OAuth2::Provider.logger || (rack_env && rack_env['rack.logger'])
+      return unless logger
+
+      extra = (@logging_context && @logging_context.merge(extra)) || extra
       extra[:level] ||= :info
-      OAuth2::Provider.logger.add SEVERITY_MAPPING[extra[:level]], format_log_message(message)
+      extra[:request_id] ||= (rack_env && rack_env['http_x_request_id']) || 'N/A'
+
+      logger.add SEVERITY_MAPPING[extra[:level]], format_log_message(message, extra)
       nil
     end
 
     private
-    def format_log_message(message)
-      ["[OAUTH2-PROVIDER]", Time.now.utc.strftime("%F %T"), message].join(" ") 
+
+    def format_log_message(message, extra = {})
+      [ "[#{extra[:component] || 'OAUTH2-PROVIDER'}]", extra[:request_id] || "N/A", Time.now.iso8601, '-', message].join(" ") 
     end
   end
 end
